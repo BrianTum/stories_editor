8d43c2eb8b0bc06f70458ae139f95af07ecdab20
BrianTum <brianktum@gmail.com>
2023-11-03 17:46:31 +0300

    centering filters check icon

--- lib/src/presentation/filters/filters.dart
+++ lib/src/presentation/filters/filters.dart
@@ -47,83 +47,79 @@ class _FiltersState extends State<Filters> {
   Widget build(BuildContext context) {
     return Consumer<ControlNotifier>(
         builder: (context, controlNotifier, child) {
-      return Row(
-        children: [
-          // Expanded(
-          //   flex: 1,
-          //   child: Center(
-          //     child: ColorFiltered(
-          //       child: Container(
-          //         color: Colors.pink,
-          //         child: Image.asset('assets/images/img.png'),
-          //       ), // Replace with your own image widget
-          //       colorFilter: ColorFilters.getColorFilter(
-          //           FilterType.values[selectedIndex])!,
-          //     ),
-          //   ),
-          // ),
-          Expanded(
-            flex: 4,
-            child: ListView.separated(
-              scrollDirection: Axis.horizontal,
-              controller: _scrollController,
-              itemCount: FilterType.values.length,
-              separatorBuilder: (BuildContext context, int index) {
-                return const SizedBox(
-                  width: 16.0,
-                ); // Adjust the width according to your desired spacing
-              },
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: selectedIndex == index
-                          ? Colors.white
-                          : Colors.transparent, // Set the border color here
-                      width: 5.0, // Set the border thickness here
+      return SizedBox(
+        child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          controller: _scrollController,
+          itemCount: FilterType.values.length,
+          separatorBuilder: (BuildContext context, int index) {
+            return const SizedBox(
+              width: 10.0,
+            ); // Adjust the width according to your desired spacing
+          },
+          itemBuilder: (BuildContext context, int index) {
+            return Center(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedIndex = index;
+                  });
+                  Provider.of<ControlNotifier>(context, listen: false)
+                      .filterIndex = index;
+                },
+                child: Stack(
+                  children: [
+                    ColorFiltered(
+                      child: controlNotifier.mediaPath != ''
+                          ? Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: selectedIndex == index
+                                      ? Colors.white
+                                      : Colors
+                                          .transparent, // Set the border color here
+                                  width: 5.0, // Set the border thickness here
+                                ),
+                              ),
+                              child: Image.file(
+                                File(controlNotifier.mediaPath),
+                                filterQuality: FilterQuality.high,
+                              ),
+                            )
+                          : Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: selectedIndex == index
+                                      ? Colors.white
+                                      : Colors
+                                          .transparent, // Set the border color here
+                                  width: 5.0, // Set the border thickness here
+                                ),
+                              ),
+                              child: Image.asset('assets/images/img.png'),
+                            ), // Replace with your own image widget
+                      colorFilter: CustomColorFilters.getFilter(
+                          FilterType.values[index]),
                     ),
-                  ),
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedIndex = index;
-                      });
-                      Provider.of<ControlNotifier>(context, listen: false)
-                          .filterIndex = index;
-                    },
-                    child: Stack(
-                      children: [
-                        ColorFiltered(
-                          child: controlNotifier.mediaPath != ''
-                              ? Image.file(
-                                  File(controlNotifier.mediaPath),
-                                  filterQuality: FilterQuality.high,
-                                )
-                              : Image.asset(
-                                  'assets/images/img.png'), // Replace with your own image widget
-                          colorFilter: CustomColorFilters.getFilter(
-                              FilterType.values[index]),
-                        ),
-                        selectedIndex == index
-                            ? const Positioned(
-                                top: 0,
-                                bottom: 0,
-                                left: 0,
-                                right: 0,
-                                child: Icon(
-                                  Icons.check_sharp,
-                                  size: 60,
-                                  color: Colors.white,
-                                ))
-                            : const Center()
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+                    selectedIndex == index
+                        ? const Positioned(
+                            bottom: 0,
+                            right: 0,
+                            top: 0,
+                            left: 0,
+                            child: Icon(
+                              Icons.check_sharp,
+                              size: 40,
+                              color: Colors.white,
+                            ),
+                          )
+                        : const Center()
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       );
     });
   }
